fe4236480be3593d7f2c2b1e177c78d8364e5d64
pdsuwwz <pdsu.wwz@foxmail.com>
2021-07-05 10:50:13 +0800

    chore: fix pm2 script path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
   apps: [
     {
       name,
-      script: path.resolve(__dirname, './dist/main.js'),
+      script: path.resolve(__dirname, './dist/bundle.cjs.js'),
       instances: require('os').cpus().length,
       autorestart: true,
       watch: true,
