ddafc7e1a2394314d0b676d421d993d97ec9d6a2
pdsuwwz <pdsu.wwz@foxmail.com>
2021-07-13 18:46:28 +0800

    feat(pdf): add custom watermark for pdf file

---
+++ src/utils/index.ts
@@ -0,0 +1,20 @@
+import { networkInterfaces } from 'os'
+
+
+export function getLocalAddress (): any {
+  const nets = networkInterfaces()
+  const results = Object.create(null) // Or just '{}', an empty object
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      if (net.family === 'IPv4' && !net.internal) {
+        if (!results[name]) {
+          results[name] = []
+        }
+        results[name].push(net.address)
+      }
+    }
+  }
+  return results
+}
