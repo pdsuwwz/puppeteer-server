36770e3022b2806c22ed7b7c89086864846821de
pdsuwwz <pdsu.wwz@foxmail.com>
2022-10-13 11:27:13 +0800

    🔥 fix: replace context type for pdflist

--- src/controllers/generate-combine-pdf.ts
+++ src/controllers/generate-combine-pdf.ts
@@ -1,19 +1,26 @@
-import Koa from 'koa'
+import Koa, { Request } from 'koa'
 
 import GeneratePdfService from '@/services/generate-pdf'
 import GenerateCombinePdfService from '@/services/generate-combine-pdf'
 import { RequestBody } from '@/controllers/generate-pdf'
 
+interface RequestCombinePDF extends Request {
+  body: {
+    pdfList: RequestBody[]
+  }
+}
+
+interface ContextCombinePDF extends Koa.Context {
+  request: RequestCombinePDF
+}
+
+
 class GenerateCombinePdfController {
   private pdfService: GeneratePdfService = new GeneratePdfService()
   private service: GenerateCombinePdfService = new GenerateCombinePdfService()
 
-  generate = async (ctx: Koa.Context) => {
-    let {
-      pdfList = []
-    }: {
-      pdfList: RequestBody[]
-    } = ctx.request.body
+  generate = async (ctx: ContextCombinePDF) => {
+    let { pdfList = [] } = ctx.request.body
 
     pdfList = pdfList.filter((pdfItem) => {
       return pdfItem.url
