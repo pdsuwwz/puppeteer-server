83ca7ebd1fdbbdac2063dfb1af573fadfc6d988c
Wisdom <pdsu.wwz@foxmail.com>
2023-07-28 16:16:58 +0800

    feat: support cron restart (#82)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,7 @@ module.exports = {
       instances: require('os').cpus().length,
       autorestart: true,
       watch: true,
+      cron_restart: '0 3 * * *',
       env_production: {
         NODE_ENV: 'production',
         PORT: 8080
